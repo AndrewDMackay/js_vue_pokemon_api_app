c30835ce675d74b49ca3ee21be02e5431e38c7d0
Andrew Mackay <andrewmackayd@gmail.com>
2021-12-14 14:14:43 +0000

    AM, 4th commit, main

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -1,16 +1,26 @@
 <template>
     <div id="pokemon-list">
         <h2>Pokemon List</h2>
+        <ul>
+            <pokemon-list-item 
+            v-for="(pokemon, index) in allPokemon" 
+            :key="index" :pokemon="pokemon"/>
+        </ul>
     </div>
 </template>
 
 
 
 <script>
+import PokemonListItem from './PokemonListItem.vue';
 export default {
     name: "PokemonList",
-    props: [ "allPokemon" ]
-}
+    props: [ "allPokemon" ],
+    components: {
+        "pokemon-list-item": PokemonListItem,
+    },
+};
+
 </script>
 
 
---
+++ src/components/PokemonListItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="pokemon-list-item">
+      <li>{{ pokemon.name }}</li>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+    name: "PokemonListItem",
+    props: ["pokemon"],
+
+}
+</script>
+
+
+
+<style>
+
+</style>
+
